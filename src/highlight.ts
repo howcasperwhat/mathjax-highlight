dbd9692324fd1c63abfdb26ebf2692b3c3e001ac
Casper Huang <casper.w.huang@qq.com>
2025-06-29 14:43:39 +0800

    chore: update

--- src/highlight.ts
+++ src/highlight.ts
@@ -7,28 +7,32 @@ export function useHighlight() {
   if (!base.value)
     return
 
-  watch(exposed, async () => {
-    const dir = 'syntaxes'
-    const files = await workspace.fs.readDirectory(join(dir))
-    for (const [file, type] of files) {
-      if (type === 1 && file !== 'litemath.json') {
-        workspace.fs.delete(join(dir, file))
+  watch(
+    exposed,
+    async () => {
+      const dir = 'syntaxes'
+      const files = await workspace.fs.readDirectory(join(dir))
+      for (const [file, type] of files) {
+        if (type === 1 && file !== 'litemath.json') {
+          workspace.fs.delete(join(dir, file))
+        }
       }
-    }
 
-    for (const [lang, opt] of Object.entries(exposed.value!)) {
-      const tml = createTextMateLanguage(lang, opt)
+      for (const [lang, opt] of Object.entries(exposed.value!)) {
+        const tml = createTextMateLanguage(lang, opt)
+        workspace.fs.writeFile(
+          join(dir, `formula.${lang}.json`),
+          JSONBuffer(tml),
+        )
+      }
+
+      const pack = JSON.parse((await workspace.fs.readFile(join('package.json'))).toString())
+      pack.contributes.grammars = createGrammars(Object.keys(exposed.value!))
       workspace.fs.writeFile(
-        join(dir, `formula.${lang}.json`),
-        JSONBuffer(tml),
+        join('package.json'),
+        JSONBuffer(pack),
       )
-    }
-
-    const pack = JSON.parse((await workspace.fs.readFile(join('package.json'))).toString())
-    pack.contributes.grammars = createGrammars(Object.keys(exposed.value!))
-    workspace.fs.writeFile(
-      join('package.json'),
-      JSONBuffer(pack),
-    )
-  }, { immediate: true })
+    },
+    { immediate: true }
+  )
 }
